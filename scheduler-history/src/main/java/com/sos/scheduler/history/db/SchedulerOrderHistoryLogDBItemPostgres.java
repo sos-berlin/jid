3ed6aa968e44a7e2f481e1ba74fdac8d3198c18f
Uwe Risse <uwe.risse-git@sos-berlin.com>
2016-10-28 17:21:27 +0200

    JOC_Cockpit Postgres log

---
+++ scheduler-history/src/main/java/com/sos/scheduler/history/db/SchedulerOrderHistoryLogDBItemPostgres.java
@@ -0,0 +1,60 @@
+package com.sos.scheduler.history.db;
+
+import java.io.IOException;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import com.sos.hibernate.classes.DbItem;
+import com.sos.scheduler.history.classes.SOSStreamUnzip;
+
+@Entity
+@Table(name = "SCHEDULER_ORDER_HISTORY")
+public class SchedulerOrderHistoryLogDBItemPostgres extends DbItem {
+
+    private byte[] log;
+    private Long historyId;
+
+    public SchedulerOrderHistoryLogDBItemPostgres() {
+    }
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "`HISTORY_ID`", nullable = false)
+    public Long getHistoryId() {
+        return historyId;
+    }
+    
+    @Column(name = "`HISTORY_ID`", nullable = false)
+    public void setHistoryId(final Long id) {
+        historyId = id;
+    }
+
+
+    @Column(name = "`LOG`", nullable = true)
+    public byte[] getLog() {
+        return log;
+    }
+
+    @Column(name = "`LOG`", nullable = true)
+    public void setLog(final byte[] log) {
+        this.log = log;
+    }
+
+    @Transient
+    public String getLogAsString() throws IOException {
+        if (log == null) {
+            return null;
+        } else {
+            SOSStreamUnzip SOSUnzip = new SOSStreamUnzip(log);
+            return SOSUnzip.unzip2String();
+        }
+    }
+    
+ 
+}
---
+++ scheduler-history/src/main/java/com/sos/scheduler/history/db/SchedulerTaskHistoryLogDBItemPostgres.java
@@ -0,0 +1,52 @@
+package com.sos.scheduler.history.db;
+
+import java.io.IOException;
+
+
+import javax.persistence.*;
+
+import com.sos.hibernate.classes.DbItem;
+import com.sos.scheduler.history.classes.SOSStreamUnzip;
+
+@Entity
+@Table(name = "SCHEDULER_HISTORY")
+public class SchedulerTaskHistoryLogDBItemPostgres extends DbItem {
+
+    private Long id;
+    private byte[] log;
+
+    public SchedulerTaskHistoryLogDBItemPostgres() {
+     }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "`ID`")
+    public Long getId() {
+        return id;
+    }
+
+    @Column(name = "`ID`")
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "`LOG`", nullable = true)
+    public byte[] getLog() {
+        return log;
+    }
+
+    @Column(name = "`LOG`", nullable = true)
+    public void setLog(final byte[] log) {
+        this.log = log;
+    }
+    
+    @Transient
+    public String getLogAsString() throws IOException {
+        if (log == null) {
+            return null;
+        } else {
+            SOSStreamUnzip SOSUnzip = new SOSStreamUnzip(log);
+            return SOSUnzip.unzip2String();
+        }
+    }
+}
